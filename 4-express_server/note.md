a9c7f9dcb07a651582c4c0e27d47cf3f5a242593
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-12-09 12:26:34 +0200

    secret project

--- 4-express_server/note.md
+++ 4-express_server/note.md
@@ -12,3 +12,36 @@ sudo ldof -i -P | grep LISTEN
 ```
 ## closing  a port 
 - https://dev.to/sylwiavargas/how-to-properly-close-a-port-2p36
+
+
+## FULL template of index
+
+```js
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+```
+
+## Create custom iddleware
+
+```js
+const bandNameGen = (req, res, next) => {
+  bandName = `${req.body.pet}${req.body.city}`;
+  next();
+};
+```
--- 5-secrets/index.js
+++ 5-secrets/index.js
@@ -1,3 +1,41 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+const PASS = "ILoveProgramming";
+let auth = false;
+
+const checkPass = (req, res, next) => {
+  if (req.body.password === PASS) {
+    auth = true;
+  } else {
+    auth = false;
+  }
+  next();
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(checkPass);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.post("/check", (req, res) => {
+  if (auth) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
