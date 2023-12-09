3550c71b95756cf1abcdf16cd848b6ceabf12df4
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-12-09 13:54:40 +0200

    ejs tags exercise complete

--- 6-daily_advise_EJS/index.js
+++ 6-daily_advise_EJS/index.js
@@ -5,14 +5,17 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
-let isWeekDay = false;
+let day;
+let adv;
+
 const checkDay = (req, res, next) => {
-  const day = new Date().getDay();
-  console.log(day);
-  if (day > 0 && day < 6) {
-    isWeekDay = true;
+  const today = new Date().getDay();
+  if (today > 0 && today < 6) {
+    day = "a weekday";
+    adv = "time to work hard";
   } else {
-    isWeekDay = false;
+    day = "the weekend";
+    adv = "time to rest good";
   }
   next();
 };
@@ -21,7 +24,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(checkDay);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.ejs");
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<em> This is some em text. </em>",
+  };
+  res.render(__dirname + "/views/index.ejs", {
+    dayType: day,
+    advice: adv,
+    data,
+  });
 });
 
 app.listen(port, () => {
---
+++ 6-daily_advise_EJS/notes.md
@@ -0,0 +1,9 @@
+# EJS tags
+<%# Variables JS with output %>
+<%= %>
+<%# Execute JS with NO output %>
+<% %>
+<%# Render HTML%>
+<%- %>
+<%#- insert another ejs file %>
+<%-include("") %>
