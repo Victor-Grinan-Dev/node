505665ed3b6f01f35f480e28f6225b4f4cf118ae
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-12-18 14:40:06 +0200

    initial steps, all views available

--- 21-Secrets - Starting Code/app.js
+++ 21-Secrets - Starting Code/app.js
@@ -11,11 +11,11 @@ const db = new pg.Client({
   password: "vitismanXD2019",
   port: 5432,
 });
-db.connect();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+const is_auth = false;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -28,6 +28,41 @@ app.get("/", (req, res) => {
   res.render(__dirname + "/views/home.ejs", {
     data,
   });
+});
+
+app.get("/login", (req, res) => {
+  res.render(__dirname + "/views/login.ejs");
+});
+
+app.post("/login", (req, res) => {
+  db.connect();
+  //check user exist...
+  if (is_auth) res.redirect(__dirname + "/views/secrets.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render(__dirname + "/views/register.ejs");
+});
+
+app.post("/register", (req, res) => {
+  //create new user ...
+  if (is_auth) res.render(__dirname + "/views/secrets.ejs");
+});
+
+app.get("/submit", (req, res) => {
+  if (is_auth) res.render(__dirname + "/views/submit.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  // POST TO secret to DB...
+  try {
+  } catch (error) {}
+  res.redirect(__dirname + "/views/secrets.ejs");
+});
+
+app.get("/logout", (req, res) => {
+  //reset all keys and auth to false
+  res.redirect("/");
 });
 
 app.listen(port, () => {
